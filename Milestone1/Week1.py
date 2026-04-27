b6d226b7ffe71921d2594f3a5baed81e8c8d6764
Lerra <lr.lmdelacruz@mmdc.mcl.edu.ph>
2026-04-27 21:25:35 +0800

    Add week 1 CTF Solution

--- Milestone1/Week1.py
+++ Milestone1/Week1.py
@@ -2,4 +2,19 @@
 # Team C / MER/C : Dela Cruz, L.M., Tubera, K.G., & Samaniego, M.Y.
 
 #Week 1 - Flagged for Extraction
+import re
+
+#Read the file , line by line
+with open('CTF-W1_mystery_text_file.txt', 'r') as f:
+    content = f.read()
+    flags = re.findall(r'FLAG\{.*?\}',content)
+
+#Find the unique message
+flag_counts = {}
+for f in flags:
+    flag_counts[f] = flag_counts.get(f,0) + 1
+
+for f, count in flag_counts.items():
+    print(f"{f} = {count}")
+
 
