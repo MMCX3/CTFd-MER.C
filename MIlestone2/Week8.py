a50c2a5462f926f381a6af7a0df5cfbce4e62794
Lerra <lr.lmdelacruz@mmdc.mcl.edu.ph>
2026-05-05 11:04:31 +0800

    Revert "Add my Week2 file"
    This reverts commit a74d35744d055a37eb93bb1ce1bbe753f6540570.

---
+++ MIlestone2/Week8.py
@@ -0,0 +1,5 @@
+# MO-IT143 - Ethical Hacking S3101 | A.Y. 2025 - 2026
+# Team C / MER/C : Dela Cruz, L.M., Tubera, K.G., & Samaniego, M.Y.
+
+#Week 8 - Web Traffic Inspector
+
---
+++ MIlestone2/Week9&10.py
@@ -0,0 +1,5 @@
+# MO-IT143 - Ethical Hacking S3101 | A.Y. 2025 - 2026
+# Team C / MER/C : Dela Cruz, L.M., Tubera, K.G., & Samaniego, M.Y.
+
+#Weeks 9 & 10 - Milestone 2 - Trace the Code
+
