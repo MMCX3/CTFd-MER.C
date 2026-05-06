87c661dd0dd68a1c5c7e287ac2d5be7ae0a1d11d
MYSTRi-DECiPHER <devcphr@gmail.com>
2026-05-07 03:32:47 +0800

    added file hierarchy.

--- Milestone1/Kyle-Personal/Week2_Tubera.py
+++ Milestone1/Kyle-Personal/Week2_Tubera.py
@@ -5,7 +5,7 @@
 import re
 import base64
 import json
-
+ 
 with open('Milestone1/Milestone1-Files/CTF-W2_expanded_security_logs.json', 'r') as f:
     logs = json.load(f)
 # Sir. JR suggested finding Base64 strings using regex: re.findall(r'[A-Za-z0-9+/]{64,}[=]{0,2}')
